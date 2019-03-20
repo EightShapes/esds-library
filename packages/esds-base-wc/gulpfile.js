6a597995ca3a3837813f0e93b4d3811eddbe9a9e
Kevin Powell <kevin@eightshapes.com>
2019-03-20 16:26:05 -0500

    Add rollup gulp task to base component

--- packages/esds-base-wc/gulpfile.js
+++ packages/esds-base-wc/gulpfile.js
@@ -1,1 +1,27 @@
 const gulp = require('esds-build');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+
+const rollupInputOptions = {
+  input: './scripts/esds-base-wc.js',
+  plugins: [resolve()]
+};
+
+const rollupOutputOptions = {
+  compact: true,
+  sourcemap: true,
+  file: './dist/esds-base-wc.js',
+  format: 'esm'
+};
+
+gulp.task('scripts:rollup', async() => {
+  const bundle = await rollup.rollup(rollupInputOptions);
+  await bundle.write(rollupOutputOptions);
+});
+
+gulp.task('esds-hook:pre:build:all', gulp.series('scripts:rollup'));
+
+// Watch base WC for changes and re-rollup
+gulp.task('watch:scripts:rollup', function () {
+    return gulp.watch(['./scripts/**/*.js'], gulp.parallel('scripts:rollup'));
+});
--- packages/esds-base-wc/scripts/esds-base-wc.js
+++ packages/esds-base-wc/scripts/esds-base-wc.js
@@ -1,7 +1,10 @@
 import { LitElement, html, render, directive, ifDefined, unsafeHTML } from './lit-exports-base.js';
 
 class EsdsBaseWc extends LitElement {
-
+  constructor() {
+    super();
+    this.cheese = 'SPELL IT RIGHT';
+  }
 }
 
 export { EsdsBaseWc, LitElement, render, html, directive, ifDefined, unsafeHTML };
