eaae4bb84bbd09ca8e21e52cca19b8a9336eed60
Kevin Powell <kevin@eightshapes.com>
2019-04-05 15:01:19 -0500

    Add esds-base-wc dist build

--- packages/esds-base-wc/scripts/esds-base-wc.js
+++ packages/esds-base-wc/scripts/esds-base-wc.js
@@ -13,10 +13,10 @@
 // }
 
 // import "@babel/polyfill";
-import { LitElement, html } from '../node_modules/lit-element/lit-element.js';
-// import { html, directive, render } from '../node_modules/lit-html/lit-html.js';
-// import { ifDefined } from '../node_modules/lit-html/directives/if-defined.js';
-// import { unsafeHTML } from '../node_modules/lit-html/directives/unsafe-html.js';
+import { LitElement } from '../node_modules/lit-element/lit-element.js';
+import { html, directive, render } from '../node_modules/lit-html/lit-html.js';
+import { ifDefined } from '../node_modules/lit-html/directives/if-defined.js';
+import { unsafeHTML } from '../node_modules/lit-html/directives/unsafe-html.js';
 
 class EsdsBaseWc extends LitElement {
   constructor(componentName = '') {
@@ -42,6 +42,6 @@ class EsdsBaseWc extends LitElement {
   }
 }
 
-// export { EsdsBaseWc, LitElement, render, html, directive, ifDefined, unsafeHTML };
+export { EsdsBaseWc, LitElement, render, html, directive, ifDefined, unsafeHTML };
 
 customElements.define('test-component', EsdsBaseWc);
