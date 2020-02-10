353de162c0b0e260ad44253ab1c0d9ba86382369
Kevin Powell <kevin@eightshapes.com>
2020-02-10 09:59:49 -0600

    Update polyfill dependency on test pages

--- components/esds-icon/rollup.config.ie.js
+++ components/esds-icon/rollup.config.ie.js
@@ -1,5 +1,4 @@
 // This rollup config is used to bundle and compile a version of the component for IE11
-import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
@@ -10,12 +9,11 @@ export default {
     format: 'cjs',
   },
   plugins: [
-    resolve(),
-    commonjs(),
     babel({
       presets: [
         ['@babel/env', { modules: false }], // Putting this in a separate .babelrc file doesn't seem to work
       ],
     }),
+    resolve(),
   ],
 };
--- components/esds-icon/src/esds-icon-entry-legacy.js
+++ components/esds-icon/src/esds-icon-entry-legacy.js
@@ -1,3 +1,2 @@
-import 'core-js/stable';
 import 'regenerator-runtime/runtime'; // This is only needed for IE11, create a model to remove this or only inject it for IE11
 import './esds-icon-entry.js';
