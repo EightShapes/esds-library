99595f6f16eca5d0774a3890d319c18e3d144c9a
Kevin Powell <kevin@eightshapes.com>
2020-02-10 11:34:57 -0600

    Add root level script to list all eightshapes component dependencies

---
+++ dependency-tree.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// Lifted from: https://github.com/lerna/lerna/pull/1970#issuecomment-472184926
+
+const { getPackages } = require('@lerna/project');
+const PackageGraph = require('@lerna/package-graph');
+
+(async function () {
+  const cwd = process.cwd(); // or process.argv?
+  const pkgs = await getPackages(cwd);
+  const graph = new PackageGraph(pkgs);
+  const json = {};
+
+  graph.forEach((node, name) => {
+    if (name.startsWith('@eightshapes')) {
+      json[name] = [
+        ...node.localDependencies.keys(),
+      ].sort();
+    }
+  });
+
+  console.log(JSON.stringify(json, null, 2));
+})();
