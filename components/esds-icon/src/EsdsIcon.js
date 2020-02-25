e8b402a47bd3554d5453652ee38c349253016f57
Kevin Powell <kevin@eightshapes.com>
2020-02-25 17:11:01 -0500

    Add jsdoc sample code

--- components/esds-icon/src/EsdsIcon.js
+++ components/esds-icon/src/EsdsIcon.js
@@ -3,11 +3,28 @@ import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
 import { EsdsIconEightshapesMark } from '@eightshapes/esds-icons';
 import styles from './esds-icon-styles.js';
 
+/**
+ * @slot just-for-fun - This is the just-for-fun slot.
+ */
 export class EsdsIcon extends LitElement {
   static get properties() {
     return {
+      /*
+       * Sets height and width of the icon
+       * @type {'small'|'medium'|'large'}
+       * @values ['small', 'medium', 'large']
+       */
       size: { type: String },
+      /*
+       * Adds a visibly hidden `<title>` element with a corresponding `aria-labelledby` tag
+       * when provided. Used only when icons stand alone without other context.
+       */
       title: { type: String },
+      /*
+       * Determines which icon will be shown. When leveraging the Icon sprite, the id of the
+       * sprite symbol should be provided (ex: `#angle-right`). When using ES6 modules, pass the
+       * name of the module (ex: `EsdsIconAngleRight`). See usage examples below.
+       */
       use: { type: String },
     };
   }
@@ -24,18 +41,15 @@ export class EsdsIcon extends LitElement {
     return this;
   }
 
-  loadSpecialIconMethod() {
-    return true;
-  }
-
   render() {
     return svg`
       <style>
         ${styles}
       </style>
-      <svg class="esds-icon esds-icon--${this.size}" xmlns="http://www.w3.org/2000/svg">
+      <svg class="esds-icon esds-icon--${this.size}" xmlns="http://www.w3.org/2000/svg"><span>
         ${this.use.indexOf('#') === 0 ? svg`<use href="${this.use}"/>` : unsafeSVG(this.use)}
       </svg>
+      <slot name="just-for-fun"></slot>
     `;
   }
 }
