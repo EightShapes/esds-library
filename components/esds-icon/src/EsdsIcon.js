92f888ad97353d84437a02d2508485e5c4c11fff
Kevin Powell <kevin@eightshapes.com>
2020-02-27 23:01:41 -0500

    Add icon size tokens

--- components/esds-icon/src/EsdsIcon.js
+++ components/esds-icon/src/EsdsIcon.js
@@ -1,5 +1,6 @@
 import { svg, LitElement } from 'lit-element';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
+import { ifDefined } from 'lit-html/directives/if-defined';
 import { EsdsIconStopwatch } from '@eightshapes/esds-icons';
 import styles from './esds-icon-styles.js';
 
@@ -26,26 +27,61 @@ export class EsdsIcon extends LitElement {
     };
   }
 
+  /*
+   * Used to link the <title> and <svg> elements via a unique aria-labelledby -> id value
+   */
+  static get titleId() {
+    return `esds-icon-${Date.now()}${Math.random()
+      .toString()
+      .slice(2)}`;
+  }
+
   constructor() {
     super();
     this.use = EsdsIconStopwatch;
+    this.size = 'medium';
   }
 
   /*
    * @ignore
    */
   createRenderRoot() {
-    return this;
+    return this; // Prevents lit-element from rendering in shadowDOM
+  }
+
+  /*
+   * Private
+   * If the title prop is set, render a <title> element within the SVG which is linked via the aria-labelledby attribute to the parent <svg>
+   */
+  _renderTitle(titleId) {
+    return svg`${this.title ? svg`<title id="${titleId}">${this.title}</title>` : ''}`;
+  }
+
+  /*
+   * Private
+   * If the use prop contains a '#', a sprite sheet and symbol reference is being used. Add a <use> element to reference the symbol.
+   * If the use prop does not contain a '#', a raw SVG string is being used. Render it directly as unsafeSVG content
+   */
+  _renderUse() {
+    return svg`${this.use.includes('#') ? svg`<use href="${this.use}"/>` : unsafeSVG(this.use)}`;
   }
 
   render() {
+    const titleId = this.title ? this.constructor.titleId : undefined;
+
     return svg`
       <style>
         ${styles}
       </style>
-      <svg class="esds-icon esds-icon--${this.size}" xmlns="http://www.w3.org/2000/svg">
-        ${this.title ? svg`<title>${this.title}</title>` : ''}
-        ${this.use.indexOf('#') === 0 ? svg`<use href="${this.use}"/>` : unsafeSVG(this.use)}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="esds-icon esds-icon--${this.size}"
+        aria-labelledby="${ifDefined(titleId)}"
+        aria-hidden="${this.title ? 'false' : 'true'}"
+        role="${this.title ? 'img' : 'presentation'}"
+        >
+        ${this._renderTitle(titleId)}
+        ${this._renderUse()}
       </svg>
     `;
   }
