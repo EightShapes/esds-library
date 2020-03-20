eace90d67afb87e8e4b88a8ebacdb2b4e85bdef8
Kevin Powell <kevin@eightshapes.com>
2020-03-19 19:56:38 -0500

    Add thumbnail props table and remove incorrect eightshapes.com card example

--- test/storybook/.storybook/preview.js
+++ test/storybook/.storybook/preview.js
@@ -10,11 +10,13 @@ import { withA11y } from '@storybook/addon-a11y';
 // TODO: Make this more scalable
 import cardDocs from '@eightshapes/esds-card/documentation/esds-card-docs.json';
 import iconDocs from '@eightshapes/esds-icon/documentation/esds-icon-docs.json';
+import thumbnailDocs from '@eightshapes/esds-thumbnail/documentation/esds-thumbnail-docs.json';
 import '../stories/styles/annotation-styles.scss';
 import '../stories/styles/hostile-styles.scss';
 
 const customElements = cardDocs;
 customElements.tags.push(...iconDocs.tags)
+customElements.tags.push(...thumbnailDocs.tags)
 
 console.log(customElements);
 
