28d4f8163e17042af0bae05016e46dec955132eb
Kevin Powell <kevin@eightshapes.com>
2020-08-25 15:14:51 -0500

    Upgrade build tooling for all components

--- components/esds-background-demo/karma.conf.js
+++ components/esds-background-demo/karma.conf.js
@@ -11,14 +11,25 @@ module.exports = config => {
         //
         // npm run test -- --grep test/foo/bar.test.js
         // npm run test -- --grep test/bar/*
-        { pattern: config.grep ? config.grep : 'test/**/*.test.js', type: 'module' },
+        {
+          pattern: config.grep ? config.grep : 'test/**/*.test.js',
+          type: 'module',
+        },
+        // Watch the source web component for changes
+        { pattern: 'src/**/*-entry.js', type: 'module', watched: true },
       ],
 
       esm: {
         nodeResolve: true,
+        preserveSymlinks: true,
       },
       // you can overwrite/extend the config further
     }),
   );
+
+  // Remove unused snapshot config that open-wc automatically adds: https://github.com/open-wc/open-wc/blob/e853364b9dde1924199f4309cf6689b3bcc6e25f/packages/testing-karma/src/create-default-config.js#L25
+  const snapshotFileIndex = config.files.findIndex(fo => fo.pattern.includes('__snapshots__'));
+  config.files.splice(snapshotFileIndex, 1);
+
   return config;
 };
--- components/esds-card/src/esds-card-entry-legacy.js
+++
@@ -1,2 +0,0 @@
-import 'regenerator-runtime/runtime'; // This is only needed for IE11
-import './esds-card-entry.js';
