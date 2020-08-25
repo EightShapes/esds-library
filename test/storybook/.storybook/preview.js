cc7f1206231c948119e30494caaded6e8baf588d
Kevin Powell <kevin@eightshapes.com>
2020-08-25 09:29:29 -0500

    Upgrade storybook to version 6

--- test/storybook/.storybook/preview.js
+++ test/storybook/.storybook/preview.js
@@ -1,12 +1,7 @@
 /* global window */
-import {
-  configure,
-  addParameters,
-  addDecorator,
-  setCustomElements,
-} from '@storybook/web-components';
+import { setCustomElements } from '@storybook/web-components';
+import * as esdsTokens from '@eightshapes/esds-tokens';
 
-import { withA11y } from '@storybook/addon-a11y';
 // TODO: Make this more scalable
 import cardDocs from '@eightshapes/esds-card/documentation/esds-card-docs.json';
 import iconDocs from '@eightshapes/esds-icon/documentation/esds-icon-docs.json';
@@ -15,31 +10,32 @@ import '../stories/styles/annotation-styles.scss';
 import '../stories/styles/hostile-styles.scss';
 
 const customElements = cardDocs;
-customElements.tags.push(...iconDocs.tags)
-customElements.tags.push(...thumbnailDocs.tags)
-
-console.log(customElements);
+customElements.tags.push(...iconDocs.tags);
+customElements.tags.push(...thumbnailDocs.tags);
 
 setCustomElements(customElements);
 
-addDecorator(withA11y);
-
-addParameters({
-  docs: {
-    iframeHeight: '200px',
-    inlineStories: false
-  },
-});
-
-// configure(require.context('../stories', true, /\.stories\.(js|mdx)$/), module);
-
-// force full reload to not reregister web components
-const req = require.context('../stories', true, /\.stories\.(js|mdx)$/);
-configure(req, module);
-if (module.hot) {
-  module.hot.accept(req.id, () => {
-    const currentLocationHref = window.location.href;
-    window.history.pushState(null, null, currentLocationHref);
-    window.location.reload();
+const backgroundColors = [];
+for (const bgName in esdsTokens['background-color']) {
+  backgroundColors.push({
+    name: bgName,
+    value: esdsTokens['background-color'][bgName],
   });
 }
+
+export const parameters = {
+  a11y: {
+    element: '#root',
+    config: {},
+    options: {},
+    manual: true,
+  },
+  docs: {
+    iframeHeight: '200px',
+    inlineStories: false,
+  },
+  backgrounds: {
+    default: backgroundColors[0].name,
+    values: backgroundColors,
+  },
+};
