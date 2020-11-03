bd1e6ce39b1b7612e50e71ffa3a6308b5f2386ca
Kevin Powell <kevin@eightshapes.com>
2020-11-03 12:48:27 -0600

    Further debugging of sink utility

--- test/storybook/stories/scripts/utilities.js
+++ test/storybook/stories/scripts/utilities.js
@@ -3,6 +3,7 @@ import { html } from 'lit-html';
 export const sinkFor = (...stories) => {
   return html`
     ${stories.map(story => {
+      console.log(story.story);
       return html`
         ${story()}
         <br />
